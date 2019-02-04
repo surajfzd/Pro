0c276a8b06468d204c10054295f124636737c6d1
surajfzd <46921392+surajfzd@users.noreply.github.com>
2019-02-04 15:46:34 +0530

    Add files via upload

---
+++ Assignment1/JunitEx/src/Maths.java
@@ -0,0 +1,8 @@
+
+public class Maths 
+{
+	public int addition(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ Assignment1/JunitEx/src/MathsTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathsTest {
+	Maths obj=new Maths();
+	@Test
+	public void Test()
+	{
+		assertEquals(8,obj.addition(4, 4));
+	}
+
+
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
